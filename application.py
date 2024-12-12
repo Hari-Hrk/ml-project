fa5fc428116662a21d78ba65ec29a12622bf8bc1
Hari-Hrk <hrk.harish87@gmail.com>
2024-12-12 16:30:51 +0530

    app.py file deleted

--- application.py
+++ application.py
@@ -42,8 +42,8 @@ def predict_datapoint():
     
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0",debug=True)
+    app.run(host="0.0.0.0")
 
 
 # if __name__ == "__main__":
-#     app.run(debug=True)
+#     app.run(debug=True)   # remove debug=true for deployement
